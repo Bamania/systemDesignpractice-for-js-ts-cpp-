627476a85cdc0113921964457b55eb4967eb5326
Baman Irani <amany29074@gmail.com>
2026-03-05 01:53:45 +0530

    low level with child process in node

--- stream.js
+++ stream.js
@@ -83,6 +83,16 @@ async function processFile(inStream){
   // basically we need to make this outStream.pipe(targetStream) signal us to finish !
 
 
+  var finalstream=outStream.pipe(targetStream)
+  return new Promise((resolve,reject)=>{
+
+    finalstream.on("end",()=>{
+      resolve()
+    })
+
+  })
+
+
 }
 
 function error(msg,includeHelp=false){
